f48495551ab4bc8bd2beec38433a211797eaa95c
Mike Gozzo <mike@smooch.io>
2017-08-08 10:16:52 -0400

    fix tests

--- src/js/services/core.js
+++ src/js/services/core.js
@@ -1,10 +1,10 @@
-import { BRAND } from 'smooch-core/lib/smooch';
+import { Smooch } from 'smooch-core/lib/smooch';
 import urljoin from 'urljoin';
 
 import { VERSION } from '../constants/version';
 
 export function core({auth, appState}) {
-    return new BRAND(auth, {
+    return new Smooch(auth, {
         serviceUrl: urljoin(appState.serverURL, 'v1'),
         headers: {
             'x-smooch-sdk': `web/${VERSION}`
